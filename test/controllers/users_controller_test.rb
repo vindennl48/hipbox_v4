83feb688e5b86cb02bb06519c8036b349dd1d552
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2018-08-22 03:20:37 +0000

    Added users controller with interface view

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get login" do
+    get users_login_url
+    assert_response :success
+  end
+
+  test "should get interface" do
+    get users_interface_url
+    assert_response :success
+  end
+
+end
