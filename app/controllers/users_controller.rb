4289e1c766eba9c4b0e87a04db9f09fff1628ade
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2018-08-22 12:44:49 +0000

    Added navbar
    Added navbar to interface screen for saving, loading and switching
    users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def interface
+    @user = current_user.email.sub(/@.+$/, '').capitalize
+    @layout_name = "default"
   end
 
 end
