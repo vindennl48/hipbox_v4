4c3433e0e3fc47ceb920bb471331ffb1d39bbfbb
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2018-09-06 03:05:39 +0000

    Fixed update only at 127 value

--- app/jobs/update_variables_job.rb
+++ app/jobs/update_variables_job.rb
@@ -13,7 +13,7 @@ class UpdateVariablesJob < ApplicationJob
           ActionCable.server.broadcast "variables_channel",
             {user_id: 0, note: notes}
           notes.each do |note|
-            $OSCRUBY.send OSC::Message.new(note.osc, 127)
+            $OSCRUBY.send OSC::Message.new(note.osc, note.value.to_i)
           end
         end
         sleep(5)
