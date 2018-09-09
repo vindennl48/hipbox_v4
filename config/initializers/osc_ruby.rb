c3a5165f24da8d3dc36143c11e2e18d0e3924b6a
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2018-09-08 22:53:00 -0400

    Now have 3way com. footsw, ableton, ruby
    Everything runs through the postgres database now

--- config/initializers/osc_ruby.rb
+++ config/initializers/osc_ruby.rb
@@ -4,6 +4,7 @@ Thread.new do
   OSC.run do
     server = OSC::Server.new 4005, '0.0.0.0'
 
+    # from fcb1010 or other external midi device
     server.add_pattern %r{variable/.*} do |*args|
       vars = args[0][1..-1].split('/')
       variable = vars[1]
@@ -20,6 +21,27 @@ Thread.new do
         ActionCable.server.broadcast "variables_channel",
           {user_id: 0, note: [note]}
         $OSCRUBY.send OSC::Message.new(note.osc, note.value.to_i)
+      end
+
+    end
+
+    # from ableton
+    server.add_pattern %r{midi/.*} do |*args|
+      vars = args[0][1..-1].split('/')
+      chan = vars[1].to_i
+      ntype = vars[2]
+      value = args[1].to_i
+      osc = args[0][5..-1]
+
+      if chan == 15 and Note.exists? osc: osc
+        note = Note.where(osc: osc).first
+        if ntype == 'cc'
+          #puts "osc: #{osc}, note: #{note}, value: #{value}"
+          note.value = value
+          note.save
+          ActionCable.server.broadcast "variables_channel",
+            {user_id: 0, note: [note]}
+        end
       end
 
     end
